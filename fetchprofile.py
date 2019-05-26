13221d23f8efa30b43c4be42f157c04af5efdf19
Agit IŞIK <csd@hotmail.com.tr>
2019-05-27 02:28:53 +0300

    Default Changelist

--- fetchprofile.py
+++ fetchprofile.py
@@ -1,6 +1,8 @@
 from db import db
+
 db = db()
 
 profiles = db.query('SELECT * FROM `scrappers` ORDER BY `id` DESC limit 5')
 for profile in profiles:
-    print(profile)
+    link = "https://www.linkedin.com/in/" + profile[1]
+    print(link)
